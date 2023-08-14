f617f8a0c923cb0c65a8789d6c605c8267f997d1
mehran baqeri <83948822+mehranbaqeri@users.noreply.github.com>
2023-08-14 21:47:20 +0330

    constructor func & factory func

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const person = {name:"mehran",age:33 , city:"behshahr"};
+const person = { name: "mehran", age: 33, city: "behshahr" };
 for (let key in person) {
-   console.log(key);
+  console.log(key);
 }
-const color=["red","blue","green"];
+const color = ["red", "blue", "green"];
 for (const iterator of color) {
-console.log(iterator);
+  console.log(iterator);
 }
-let circle={
-    radius:1,location:{x:0,y:0},
-    draw(){
-        console.log("drawing circle");
-    }
+let circle = {
+  radius: 1,
+  location: { x: 0, y: 0 },
+  draw() {
+    console.log("drawing circle");
+  },
+};
+//factory function
+function createCircle(radius1) {
+  return {
+    radius1,
+    draw() {
+      console.log("drawing circle");
+    },
+  };
 }
-circle.draw();
+let circle1 = createCircle(25);
+console.log(circle1);
+//constructor function
+function CircleMaker(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("drawing circle2");
+  };
+}
+const circle3 = new CircleMaker(12);
+console.log(circle3);
