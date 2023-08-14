3f0628c18392e21fe57f6aeb290bc55e0e7e4086
mehran baqeri <83948822+mehranbaqeri@users.noreply.github.com>
2023-08-14 16:17:25 +0330

    ..

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hi");
+const person = {name:"mehran",age:33 , city:"behshahr"};
+for (let key in person) {
+   console.log(key);
+}
+const color=["red","blue","green"];
+for (const iterator of color) {
+console.log(iterator);
+}
+let circle={
+    radius:1,location:{x:0,y:0},
+    draw(){
+        console.log("drawing circle");
+    }
+}
+circle.draw();
